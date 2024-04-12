f2a5c08624485485f9d3fbf9ab5efd8d759a5889
goooseq <utkoyicg@gmail.com>
2024-04-12 19:33:14 +0300

    update server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
-require('dotenv').config(   )
+require('dotenv').config( )
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const router = require('./router/index')
 
 const PORT = process.env.PORT;
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: baseURL
+}))
+const options = {
+    dbName: 'Forial'
+};
 
 const start = async () => {
     try{
-        app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
+        await mongoose.connect(process.env.DB_URL, options)
+        app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+        mongoose.connection.on("error", err => {
+            console.log(`MongoDB connection error: ${err}`);
+        })
     }
     catch(e){
         console.log(error);
---
+++ server/models/token-model.js
@@ -0,0 +1,11 @@
+const {Schema, model} = require('mongoose');
+
+const TokenModel = new Schema({
+    user: {type: Schema.Types.ObjectId, ref:'User'},
+    refreshToken:{
+        type:String,
+        required: true
+    }
+});
+
+module.exports = model("Token", UserSchema);
